d560caf95d09125ff52062c7933037ace52aba58
ajummer <makeudreamempty@gmail.com>
2023-10-10 14:19:38 +0300

    add bcrypt salt constant

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,8 @@ const constants = {
   PORT: 3000,
   // REMINDER !! Change the last part with the proper database
   url: "mongodb://localhost:27017/petstagram",
+  // bcrypt salt rounds 
+  SALT: 10,
 };
 
 module.exports = constants;
