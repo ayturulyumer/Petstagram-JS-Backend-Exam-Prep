60a2ddba0814062fff7257aee6f486fb15e4d7ed
ajummer <makeudreamempty@gmail.com>
2023-10-10 18:48:52 +0300

    Find user/check if password is valid/return token if everything is gucci

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,35 @@
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt.js");
+const { SECRET } = require("../constants.js");
 
-exports.login = (username, password) => {};
+exports.login = async (username, password) => {
+  // find user
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    throw new Error("Invalid username or password !");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw new Error("Invalid username or password !");
+  }
+
+  // create the payload
+  const payload = {
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+  };
+
+  // generate the token
+  const token = await jwt.sign(payload, SECRET, { expiresIn: "2d" });
+
+  // if everything goes right return the generated token
+  return token;
+};
 
 exports.register = async (userData) => {
   const user = await User.exists({ username: userData.username });
