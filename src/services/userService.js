2a2f715bc973aacb0a40ec3529dbdece122c64bb
ajummer <makeudreamempty@gmail.com>
2023-10-10 14:20:08 +0300

    Fix register function bug

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,7 @@ const User = require("../models/User.js");
 exports.login = (username, password) => {};
 
 exports.register = async (userData) => {
-  const user = User.findOne({ username: userData.username });
+  const user = await User.exists({ username: userData.username });
   if (user) {
     throw new Error("Username already exists !");
   }
