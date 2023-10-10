0a484b87aaccd691448001485eccddde083af324
ajummer <makeudreamempty@gmail.com>
2023-10-10 18:47:30 +0300

    Add SECRET constant

--- src/constants.js
+++ src/constants.js
@@ -2,8 +2,9 @@ const constants = {
   PORT: 3000,
   // REMINDER !! Change the last part with the proper database
   url: "mongodb://localhost:27017/petstagram",
-  // bcrypt salt rounds 
+  // bcrypt salt rounds
   SALT: 10,
+  SECRET: "37b6ea60-80aa-46ed-8263-1bf2df15c9c0",
 };
 
 module.exports = constants;
