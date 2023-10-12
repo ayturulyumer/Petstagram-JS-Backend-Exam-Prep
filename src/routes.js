34826789f8719440495806e75c8672594990b113
ajummer <makeudreamempty@gmail.com>
2023-10-12 21:03:22 +0300

    Add photoController to the routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController.js");
 const userController = require("./controllers/userController.js");
+const photoController = require("./controllers/photoController.js");
 
 router.use(homeController);
 router.use("/users", userController);
-router.get("*", (req,res) =>{
-    res.redirect("/404")
-})
+router.use("/photos", photoController);
+router.get("*", (req, res) => {
+  res.redirect("/404");
+});
 
 module.exports = router;
