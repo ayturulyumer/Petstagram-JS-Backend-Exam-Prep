d119ea4b6200fcd4aedf8466be390786214c7b17
ajummer <makeudreamempty@gmail.com>
2023-10-12 21:19:55 +0300

    Add create photo functionality with error handling

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,7 +1,19 @@
 const router = require("express").Router();
+const photoService = require("../services/photoService.js");
+const { getErrorMessage } = require("../utils/errorHelpers.js");
 
 router.get("/create", (req, res) => {
   res.render("photos/create");
+});
+
+router.post("/create", async (req, res) => {
+  const photoData = req.body;
+  try {
+    await photoService.create(photoData);
+    res.redirect("/photos");
+  } catch (err) {
+    res.render("photos/create", { error: getErrorMessage(err) });
+  }
 });
 
 module.exports = router;
