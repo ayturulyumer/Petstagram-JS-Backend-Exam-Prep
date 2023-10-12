dd9758f628d4d6e285dab86baa1d1f4f07f62721
ajummer <makeudreamempty@gmail.com>
2023-10-12 12:32:15 +0300

    Add custom error messages

--- src/models/User.js
+++ src/models/User.js
@@ -5,16 +5,16 @@ const { SALT } = require("../constants.js");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: [true,"Username is required !"],
     unique: true,
   },
   email: {
     type: String,
-    required: true,
+    required: [true,"Email is required ! !"],
   },
   password: {
     type: String,
-    required: true,
+    required: [true,"Password is required !"],
   },
 });
 
