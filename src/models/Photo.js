d8c601ac01387273dae5ab474a316c32231fbcb7
ajummer <makeudreamempty@gmail.com>
2023-10-14 14:16:10 +0300

    Change the user inside to comment to ref

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -3,37 +3,41 @@ const mongoose = require("mongoose");
 const photoSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true,"Name is required !"],
+    required: [true, "Name is required !"],
   },
   image: {
     type: String,
-    required: [true,"Image is required !"],
+    required: [true, "Image is required !"],
   },
   age: {
     type: Number,
-    required: [true,"Age is required !"],
+    required: [true, "Age is required !"],
   },
   description: {
     type: String,
-    required: [true,"Description is required !"],
+    required: [true, "Description is required !"],
   },
   location: {
     type: String,
-    required: [true,"Location is required !"],
+    required: [true, "Location is required !"],
   },
   owner: {
     type: mongoose.Types.ObjectId,
     ref: "User",
   },
   comments: [
-  {
-    userId: mongoose.Types.ObjectId,
-    message: {
-      type: String,
-      required: [true, "Comment message is required !"]
-    }
-  }
-  ]
+    {
+      user: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: "User",
+      },
+      message: {
+        type: String,
+        required: [true, "Comment message is required !"],
+      },
+    },
+  ],
 });
 
 const Photo = mongoose.model("Photo", photoSchema);
